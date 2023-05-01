31717186387792f390b5b0c4d8153a483a451671
Hunter Andersen <andersen.hunter42@gmail.com>
2023-05-01 09:10:28 -0700

    Finished Component State and Props Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
-import logo from "./logo.svg";
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+
+  constructor() {
+    //Super calls the parent constructor
+    super();
+
+    this.state = {
+      list: ["ready", "set", "go"],
+      text: "",
+    }
+
+    //Make sure that the method is bound to this class
+    //Aka, keep Texas from seceding
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.setState({
+      list: [...this.state.list, this.state.text]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
         <h1>Learning React</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React is so cool!</p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="listInput" id="listInput"
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add</button>
+        </form>
         <ul>
-          <li>Phone</li>
-          <li>Wallet</li>
-          <li>Keys</li>
+          {this.state.list.map((item, index) => {
+            return <li key={item+index}>{item}</li>;
+          })}
         </ul>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
